0482c909b8ab8328dda434f87cb9a28e09bbbd8e
Charlie Crawford <crawfordnet@icloud.com>
2018-02-16 06:09:45 -0500

    * CurrencyConverter.cs: added page for budget categories
    * MainPage.xaml.cs:
    * BudgetCategories.xaml:
    * BudgetCategories.xaml.cs:
    * CurrencyConverter.cs:

---
+++ EasyBudget.Forms/Converters/CurrencyConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace EasyBudget.Forms
+{
+    public class CurrencyConverter : IValueConverter
+    {
+        public CurrencyConverter()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
