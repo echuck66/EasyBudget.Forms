88478bd452549414b03a6a229efaad9c3bc3f1ea
Charlie Crawford <crawfordnet@icloud.com>
2018-02-20 13:31:21 -0500

    added category view page and tabs

---
+++ EasyBudget.Forms/Pages/BudgetCategoryView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EasyBudget.Forms.Pages
+{
+    public partial class BudgetCategoryView : ContentPage
+    {
+        public BudgetCategoryView()
+        {
+            InitializeComponent();
+        }
+
+        public async void OnBackClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
