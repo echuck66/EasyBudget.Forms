e79f83b8e9e223a44678cbbd65ae22a3866d3b22
Charlie Crawford <crawfordnet@icloud.com>
2018-02-23 17:21:01 -0500

    Savings Account Register XAML and event handlers

--- EasyBudget.Forms/Pages/SavingsAccountRegister.xaml.cs
+++ EasyBudget.Forms/Pages/SavingsAccountRegister.xaml.cs
@@ -12,5 +12,31 @@ namespace EasyBudget.Forms.Pages
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
         }
+
+        protected void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+
+        protected async void OnItemEdit(object sender, EventArgs e)
+        {
+
+        }
+
+        protected async void OnItemDelete(object sender, EventArgs e)
+        {
+
+        }
+
+        public async void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
+        }
+
+        protected async void OnNewItemClicked(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
